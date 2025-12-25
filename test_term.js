4d3d7081fd74401f7e1e34d9cf640b09f4000349
ferryfernando2 <ferryfernando2206@gmail.com>
2025-12-26 03:08:50 +0700

    Delete test_term.js

--- test_term.js
+++
@@ -1,5 +0,0 @@
-(async ()=>{
-  const idx = require('./indexer');
-  const r = await idx.search('Waymos', { limit: 20 });
-  console.log('results:', r.map(x => ({url: x.url, score: x.score})).slice(0,20));
-})();
