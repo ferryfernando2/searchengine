1e3af98e51694ab6bf6eb78340f5cd6895871f4d
ferryfernando2 <ferryfernando2206@gmail.com>
2025-12-26 03:06:30 +0700

    Delete scripts directory

--- scripts/inspect_schema.js
+++
@@ -1,8 +0,0 @@
-const sqlite3 = require('sqlite3');
-const { open } = require('sqlite');
-(async () => {
-  const db = await open({ filename: './data.sqlite', driver: sqlite3.Database });
-  const info = await db.all("PRAGMA table_info('docs')");
-  console.log(info);
-  await db.close();
-})();
